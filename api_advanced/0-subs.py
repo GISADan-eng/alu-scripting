aa9b334f8b6e657b83e9ac61d39b95d26a01b28c
GISADan-eng <d.gisa2@alustudent.com>
2026-03-19 10:48:13 +0000

    Fix User-Agent for Reddit API

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,7 +8,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (custom agent)"}
+    headers = {
+        "User-Agent": "linux:myredditapp:v1.0.0 (by /u/yourusername)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get("data", {}).get("subscribers", 0)
