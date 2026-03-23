22056b80fa4b611cfb2b25445658a77b24d25192
GISADan-eng <d.gisa2@alustudent.com>
2026-03-23 19:15:25 +0000

    Add 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,7 +8,9 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "python:top_ten:v1.0 (by /u/custom_agent)"}
+    headers = {
+        "User-Agent": "linux:myredditapp:v1.0.0 (by /u/yourusername)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         posts = response.json().get("data", {}).get("children", [])
